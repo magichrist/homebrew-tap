6cf30f1d9e08061b0319b4e68d92f724f8999eec
magician <161380923+magichrist@users.noreply.github.com>
2026-06-25 22:00:57 +0330

    Update weather_cli.rb

--- weather_cli.rb
+++ weather_cli.rb
@@ -1,20 +1,20 @@
 class WeatherCli < Formula
   desc "Fast CLI tool to fetch and display weather information"
   homepage "https://github.com/magichrist/weather_cli"
-  version "0.2.6"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/magichrist/weather_cli/releases/download/v#{version}/weather_cli-macos-aarch64.gz"
-      sha256 "b0f831e8f589397a83f52b62dc4fcc5cfa3ff55a44ccbf4e6d4f3299e670ae99"
+      url "https://github.com/magichrist/weather_cli/releases/download/v#{version}/weather_cli-macos-aarch64"
+      sha256 "b5848cf04d123489cca8714484f1796885c9d02db34297e39642770c0f34f114"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/magichrist/weather_cli/releases/download/v#{version}/weather_cli-linux-x86_64.gz"
-      sha256 "740c6f183321d362efe63cac932061a3297f0c2266c8642c3ac955785c636bdc"
+      url "https://github.com/magichrist/weather_cli/releases/download/v#{version}/weather_cli-linux-x86_64"
+      sha256 "5e183be5127b1952a57e9badc69436444f7a4a91d593fb11300c9da514c38bba"
     end
   end
 
